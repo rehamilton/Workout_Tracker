2b0ee8e66b1f8f222985ca012460c5ac900917b4
Roz <roz_E4@hotmail.com>
2020-06-19 20:26:46 +0800

    virtuals! duration now works

--- models/workout.js
+++ models/workout.js
@@ -1,18 +1,38 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-let workoutSchema = new Schema({
-    day: { type: Date, default: Date.now },
-    exercises: [{
-        type: {type: String, required: true},
-        name: {type: String, required: true},
-        distance: Number,
-        duration: Number, 
-        weight: Number,
-        reps: Number,
-        sets: Number
-    }],
-},{versionKey: false});
+let workoutSchema = new Schema(
+    {
+        day: { type: Date, default: Date.now },
+        exercises: [{
+            type: {type: String, required: true},
+            name: {type: String, required: true},
+            distance: Number,
+            duration: Number, 
+            weight: Number,
+            reps: Number,
+            sets: Number
+        }]
+    },
+    //add duration calculation
+    {
+        toJSON: {
+          virtuals: true
+        }
+    },
+    //stop version coming up in JSON
+    {
+        versionKey: false
+    }
+);
+
+//fix duration not calcultaing
+workoutSchema.virtual("totalDuration").get(function () {
+    // tally up sum of durations. set default to 0
+    return this.exercises.reduce((total, exercise) => {
+      return total + exercise.duration;
+    }, 0);
+});
 
 var Workout = mongoose.model("Workout", workoutSchema);
 
