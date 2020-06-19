12300982d7003ea408d37b5984538fcfeef3ffc9
Roz <roz_E4@hotmail.com>
2020-06-19 17:39:44 +0800

    htmlRoutes all in place according to public folder

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,19 @@
 var path = require("path");
 
 module.exports = function(app) {
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-  });
+    //index page
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+
+    //exercise page
+    app.get("/exercise",function (req,res){   
+        res.sendFile(path.join(__dirname,"../public/exercise.html"));
+    });
+
+    //stats page
+    app.get("/stats",function(req,res){   
+        res.sendFile(path.join(__dirname,"../public/stats.html"));
+    });
+
 };
